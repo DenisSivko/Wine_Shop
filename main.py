0e0ef9e05fc63df56fb76d7c539d631c0bbe3bc2
DenisSivko <densivko2306@yandex.ru>
2021-03-30 01:51:14 +0300

    Third commit: Code optimization

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas as pd
 
-excel_data_df = pd.read_excel('wine3.xlsx', na_values=['N/A', 'NA'],
+excel_data_df = pd.read_excel('wine2.xlsx', na_values=['N/A', 'NA'],
                               keep_default_na=False)
 
 data = excel_data_df.to_dict()
@@ -18,19 +18,18 @@ env = Environment(
 
 template = env.get_template('templates/ListWine3.html')
 
-if "Категория" in data:
+if 'Категория' in data:
     template = env.get_template('templates/ListWine2.html')
     for i in range(len(data['Категория'])):
         if data['Категория'][i] not in unique_category:
             unique_category.append(data['Категория'][i])
-
-for category in unique_category:
-    for i in range(len(data['Цена'])):
-        if data['Категория'][i] == category:
-            if data['Цена'][i] <= minimal:
-                minimal = data['Цена'][i]
-    minimal_category.append(minimal)
-    minimal = max(data['Цена'].values())
+    for category in unique_category:
+        for i in range(len(data['Цена'])):
+            if data['Категория'][i] == category:
+                if data['Цена'][i] <= minimal:
+                    minimal = data['Цена'][i]
+        minimal_category.append(minimal)
+        minimal = max(data['Цена'].values())
 
 rendered_page = template.render(
     data=data,
