37a49dc3dc970a0ef08863b6ab39e07fb8467390
DenisSivko <densivko2306@yandex.ru>
2021-03-30 01:37:08 +0300

    Second commit: Updating architecture project

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pandas as pd
 
-excel_data_df = pd.read_excel('wine2.xlsx', na_values=['N/A', 'NA'],
+excel_data_df = pd.read_excel('wine3.xlsx', na_values=['N/A', 'NA'],
                               keep_default_na=False)
 
 data = excel_data_df.to_dict()
+unique_category = []
+minimal_category = []
+minimal = max(data['Цена'].values())
+minimal_price = min(data['Цена'].values())
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-unique_category = []
-template = env.get_template('ListWine3.html')
+template = env.get_template('templates/ListWine3.html')
 
 if "Категория" in data:
-    template = env.get_template('ListWine2.html')
+    template = env.get_template('templates/ListWine2.html')
     for i in range(len(data['Категория'])):
         if data['Категория'][i] not in unique_category:
             unique_category.append(data['Категория'][i])
-
-minimal = max(data['Цена'].values())
-minimal_category = []
 
 for category in unique_category:
     for i in range(len(data['Цена'])):
@@ -31,8 +31,6 @@ for category in unique_category:
                 minimal = data['Цена'][i]
     minimal_category.append(minimal)
     minimal = max(data['Цена'].values())
-
-minimal_price = min(data['Цена'].values())
 
 rendered_page = template.render(
     data=data,
