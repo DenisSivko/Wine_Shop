12b7ec78212f8113fcc6003c8fc84fd521285ff3
DenisSivko <densivko2306@yandex.ru>
2021-03-29 21:21:54 +0300

    First commit: Wine Shop

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Wine_Shop
+﻿# Wine Shop
+## Магазин вина
+
+### Описание
 Основная задача проекта: используя шаблонизатор Jinja2 настроить перенос данных из xlsx файлов на сайт.
+### Технологии
+- flake8==3.9.0
+- importlib-metadata==3.9.1
+- Jinja2==2.11.3
+- Jinja2-Ext==0.1
+- MarkupSafe==1.1.1
+- mccabe==0.6.1
+- mypy==0.812
+- mypy-extensions==0.4.3
+- numpy==1.20.1
+- pandas==1.1.5
+- pycodestyle==2.7.0
+- pyflakes==2.3.1
+- python-dateutil==2.8.1
+- pytz==2021.1
+- six==1.15.0
+- typed-ast==1.4.2
+- typing-extensions==3.7.4.3
+- xlrd==1.2.0
+- zipp==3.4.1
+
+### Запуск проекта dev-режиме
+- Скачайте код и активируйте виртуальное окружение
+- Установите зависимости из файла requirements.txt
+```
+pip install -r requirements.txt
+``` 
+- В папке с файлом manage.py выполните команду:
+```
+python manage.py
+```
+- Перейдите на сайт по адресу [http://127.0.0.1:8000](http://127.0.0.1:8000).
+### Автор
+Сивко Денис
+
---
+++ main.py
@@ -0,0 +1,48 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import pandas as pd
+
+excel_data_df = pd.read_excel('wine2.xlsx', na_values=['N/A', 'NA'],
+                              keep_default_na=False)
+
+data = excel_data_df.to_dict()
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+unique_category = []
+template = env.get_template('ListWine3.html')
+
+if "Категория" in data:
+    template = env.get_template('ListWine2.html')
+    for i in range(len(data['Категория'])):
+        if data['Категория'][i] not in unique_category:
+            unique_category.append(data['Категория'][i])
+
+minimal = max(data['Цена'].values())
+minimal_category = []
+
+for category in unique_category:
+    for i in range(len(data['Цена'])):
+        if data['Категория'][i] == category:
+            if data['Цена'][i] <= minimal:
+                minimal = data['Цена'][i]
+    minimal_category.append(minimal)
+    minimal = max(data['Цена'].values())
+
+minimal_price = min(data['Цена'].values())
+
+rendered_page = template.render(
+    data=data,
+    unique_category=unique_category,
+    minimal_price=minimal_price,
+    minimal_category=minimal_category,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
